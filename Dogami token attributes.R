42cf8a73d74ff61bc1a4250b5ac17a91e8f25a18
Your Name <you@example.com>
2022-11-27 09:59:04 +0100

    1127 1

--- Dogami token attributes.R
+++ Dogami token attributes.R
@@ -24,7 +24,7 @@ Getquery<-Query$new()$query('link', string2)
 dogami <- conn$exec(Getquery$link, variables = variable) %>%
   fromJSON(flatten = F)
 str(dogami)
-dogami$
+
 names(dogami$data$token_attribute$attribute)
 dim(dogami$data$token_attribute$attribute)
 dim(dogami$data$token)
@@ -34,10 +34,16 @@ summary(doga_attribute$id)
 
 #So said, the API only provide with 500 records according to Objkt.com
 Sys.sleep(5)  
-list <- c(1:2)
+
+#start of the loop------
+dogami_attribute =""
+for (i in ) {
+  
+}
+list <- c(1:5)
 string1<- sprintf('"%s"', list)
 #string1[2] <- substring(string1[2],1, nchar(string1[2])-1) #Delete ","
-string1
+writeLines(toString(string1))
 
 string2 <-sprintf('query MyQuery {
   token_attribute(
@@ -58,7 +64,20 @@ string2 <-sprintf('query MyQuery {
   }
 }', toString(string1))
 
-toString(string1)
-string2
-writeLines(string2)
-writeLines(toString(string1))
+Getquery<-Query$new()$query('link', string2)
+
+dogami <- conn$exec(Getquery$link, variables = variable) %>%
+  fromJSON(flatten = F)
+
+str(dogami$data$token_attribute)
+dogami_attribute_sub <- dogami$data$token_attribute$attribute
+dogami_attribute_sub$token_id <-dogami$data$token_attribute$token$token_id
+dogami_attribute_sub$token_pk <-dogami$data$token_attribute$token_pk
+dogami_attribute <-cbind(dogami_attribute_sub)
+Sys.sleep(0.6)
+
+#end of the loop
+dogami_attribute %>% arrange(token_id, id)
+
+arrange(mtcars, cyl, disp)
+dogami_1$token$token_id
